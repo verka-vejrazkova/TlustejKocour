d6c770b0ac25819da32c36e238586b5b973b8bd5
verka-vejrazkova <71260911+verka-vejrazkova@users.noreply.github.com>
2021-12-17 09:38:21 +0100

    Přidání JS

---
+++ main.js
@@ -0,0 +1,28 @@
+let obrazky = ['TK1.jpg', 'TK2.jpg', 'TK3.jpg', 'TK4.jpg', 'TK5.jpg', 'TK6.jpg'];
+let foto = document.querySelector('#foto');
+let pocitadlo = document.querySelector('#pocitadlo')
+let index = 0;
+
+zobrazFotku(index);
+
+
+function predchoziObrazek() {
+    if (index === 0) {
+        return;
+    }
+    index--;
+    zobrazFotku(index);
+}
+
+function dalsiObrazek() {
+    if (index === obrazky.length - 1) {
+        return;
+    }
+    index++;
+    zobrazFotku(index);
+}
+
+function zobrazFotku(index) {
+    foto.src = 'obrazky/' + obrazky[index];
+    pocitadlo.textContent = obrazky[index] + ' - ' + (index + 1) + ' / ' + obrazky.length;
+}
